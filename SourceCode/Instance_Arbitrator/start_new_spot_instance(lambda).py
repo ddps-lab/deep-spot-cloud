fad9c59702ff3b6048b7f6b474c58fcc9daa8556
mjayson <mjs8612@google.com>
2017-03-02 23:53:15 +0900

    modified the start_new_spot_instance(lambda).py since lambda can be executed in any region by configuring region in lambda function

--- SourceCode/Instance_Arbitrator/start_new_spot_instance(lambda).py
+++ SourceCode/Instance_Arbitrator/start_new_spot_instance(lambda).py
@@ -1,37 +1,68 @@
 import boto3
+# import pprint
 import base64
-client=boto3.client('ec2')
 
-def lambda_handler(event, context):    # this lambda is for us-west-1 (N.California)(region)
-    az='{}'.format(event['az'])        # for input parameters az=availability zone
-    ud='{}'.format(event['ud'])        #  ud =user-data
-    
-    data= 'checkpoint-file-path:mj.bucket-1/' + ud        #mj.bucket-1 is s3 path information
-       
+# import pycurl
+# import cStringIO
+# import json
+
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.Table('g2-instance')
+
+ami_list = {'ap-northeast-1': 'ami-####', 'ap-southeast-1': 'ami-###', 'ap-southeast-2': 'ami-###',
+            'eu-central-1': 'ami-####', 'eu-west-1': 'ami-###', 'us-east-1': 'ami-###',
+            'us-west-1': 'ami-###', 'us-west-2': 'ami-###'}
+
+
+def lambda_handler(event, context):
+    ud = '{}'.format(event['ud'])
+
+    response = table.scan()
+
+    a_table = {}
+
+    for key, values in response.iteritems():
+
+        if key == "Items":
+
+            for i in values:
+                a_table[str(i[u'az'])] = float(i[u'price'])
+
+    new_az = min(a_table, key=a_table.get)  # return the key which has the lowest value
+
+    new_region = new_az[:-1]
+
+    sentence = "Starting new instance in " + new_az
+
+
+
+    client = boto3.client('ec2', region_name=new_region)
+
+    data = 'checkpoint-file-path:mj-bucket-1/' + ud
+
     response = client.request_spot_instances(
-       
+
         SpotPrice='0.65',
         InstanceCount=1,
-       
+
         Type='one-time',
-        LaunchSpecification={            
-            
-            'ImageId': 'ami-9cdb86fc',   # amis are all different in each every region 
-            'KeyName': 'macbook-mj',     
-            'InstanceType':'g2.2xlarge',
+        LaunchSpecification={
+
+            'ImageId': ami_list[new_region],
+            'KeyName': 'macbook-mj',
+            'InstanceType': 'g2.2xlarge',
             'SecurityGroups': ['mj-security'],
+
             'Placement': {
-            'AvailabilityZone': az    
+                'AvailabilityZone': new_az
             },
-            'UserData': base64.b64encode(data.encode("ascii")).decode('ascii') 
-            }
-        )
-    return "start new instance from us-west-1(N.California)"   
-    
-    
-    ## In Amazon API Gateway have to set configuration in Integration Request -> add mapping templates as "application/json" and put
-    ##   {
-    ##      "az":$input.params('az'),
-    ##      "ud":$input.params('ud')
-    ##   }
-    ## 
+            'UserData': base64.b64encode(data.encode("ascii")).decode('ascii')
+        }
+    )
+    return sentence
+
+
+
+
+
