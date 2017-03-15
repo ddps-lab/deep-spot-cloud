b3cb67339d0a59a6103464bf866ad9b1739c4428
mjayson <mjs8612@google.com>
2017-03-15 22:22:10 +0900

    updated code to sleep every 5 seconds instead of "always" and changed the time metrics from seconds to minutes when caculating elpased time

--- SourceCode/Instance_Arbitrator/migration(instance).py
+++ SourceCode/Instance_Arbitrator/migration(instance).py
@@ -1,22 +1,18 @@
 import pycurl
-import os, sys
-
+import os
 import time
-import json
 import cStringIO
-
 import datetime
 import re
-from json import dumps
+
 
 interrupt_check_url = "http://169.254.169.254/latest/meta-data/spot/termination-time"
-
 
 def check_if_migration_needed():
   buffer = cStringIO.StringIO()
   c = pycurl.Curl()
   az=get_az()
-  c.setopt(c.URL, 'xxxxx/fetch/migration?az=%22'+az+'%22')
+  c.setopt(c.URL, 'xxxx/fetch/migration?az=%22'+az+'%22')
   c.setopt(c.WRITEDATA, buffer)
   c.perform()
   c.close()
@@ -31,7 +27,7 @@ def check_if_interrupted() :
   c.close()
   body = buffer.getvalue()
   return bool(re.search('.*T.*Z', body))
-  #return True
+
 def get_az() :
   buffer = cStringIO.StringIO()
   c = pycurl.Curl()
@@ -49,7 +45,7 @@ def make_migration_notice():
 instance_time = None
 buf = cStringIO.StringIO()
 c=pycurl.Curl()
-c.setopt(c.URL, 'xxxxxxx/fetch/')
+c.setopt(c.URL, 'https://xxxxx/fetch/')
 c.setopt(c.WRITEFUNCTION, buf.write)
 c.perform()
 instance_running_time=buf.getvalue()
@@ -57,25 +53,27 @@ instance_running_time=instance_running_time[:-1]
 instance_running_time=instance_running_time[1:]
 c.close()
 
-print ("instance_running_time is")
-print (instance_running_time)
-print ("instance's running time in seconds")
+print "instance_running_time is"
+print instance_running_time
+print "instance's running time in seconds"
 instance_running_time_seconds=time.mktime(datetime.datetime.strptime(instance_running_time, "%Y-%m-%d %H:%M:%S").timetuple())
-print (instance_running_time_seconds)
+print instance_running_time_seconds
 flag=0
 while True:
+  time.sleep(5)
   elapsed=(int(time.time()-instance_running_time_seconds))
-  print ("elapsed time in seconds")
+  print "elapsed time in seconds"
   print (elapsed)
-  print ("elapsed time in minutes")
+  print "elapsed time in minutes"
   print (elapsed/60)
-  if (elapsed-3300) % 3600 == 0 and flag==0:
+  elapsed_minutes=int(elapsed/60)
+  if (elapsed_minutes-55) % 60 == 0 and flag==0:
     result=check_if_migration_needed()
     if(result=="true"):
       make_migration_notice()
       break
     flag=1
-  elif (elapsed-3300) % 3600 != 0 and flag==1:
+  elif (elapsed_minutes-55) % 60 != 0 and flag==1:
     flag=0
   elif (check_if_interrupted()):
     make_migration_notice()
