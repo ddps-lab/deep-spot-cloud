1e8fdcfdf9ecaf611483fbbe06dae8b099bfa399
mjayson <mjs8612@google.com>
2017-03-29 18:27:30 +0900

    modified the code by putting previous instance's price when new instance starts on Route's 'old price' column

--- SourceCode/DemoServer/uploading_migration_route_to_rds.py
+++ SourceCode/DemoServer/uploading_migration_route_to_rds.py
@@ -6,7 +6,15 @@ import boto3
 
 import pymysql
 
+from boto3.dynamodb.conditions import Key, Attr
+
+
 def lambda_handler(event, context):
+    p_id = None
+
+    p_az = None
+
+    p_price = ""
 
     c_instance_id = None
 
@@ -14,6 +22,26 @@ def lambda_handler(event, context):
 
     c_az = None
 
+    c_price = ""
+
+    instance_time = None
+
+    conn = pymysql.connect(host="xxxxx.us-east-1.rds.amazonaws.com", user="mj", passwd="xxx",
+                           db="migration", connect_timeout=5)
+    with conn.cursor() as cur:
+
+        cur.execute("select id, az from Route ORDER BY id DESC LIMIT 1")
+
+        rows = cur.fetchall()
+
+        item0 = rows[0]
+
+        p_id = item0[0]
+
+        p_az = item0[1]
+
+    cur.close()
+    conn.close()
     message = (event['Records'][0]['Sns']['Message'])
 
     d = ast.literal_eval(message)
@@ -22,7 +50,8 @@ def lambda_handler(event, context):
 
         if key == "region":
             c_region = value
-
+        if key == "time":
+            instance_time = value
         if key == "detail":
 
             for i, j in value.iteritems():
@@ -53,43 +82,59 @@ def lambda_handler(event, context):
                 for j in (i['Instances']):
                     c_az = j[u'Placement'][u'AvailabilityZone']
 
+    # dynamo=boto3.client('dynamodb')
 
-    dynamo=boto3.client('dynamodb')
+    dynamodb = boto3.resource('dynamodb')
 
-    c_price=""
+    table = dynamodb.Table('g2-instance')
 
-    response = dynamo.get_item(TableName='g2-instance', Key={'az':{'S':c_az}})
+    response = table.query(
+        KeyConditionExpression=Key('az').eq(p_az)
+    )
+    for i in response['Items']:
+        p_price = "$ " + i['price']
 
-    for keys, values in response.iteritems():
-        if keys == "Item":
-            for key, value in values.iteritems():
-                if key==u'price':
-                    a=value.values()
+        # =current_az_price
 
-                    for i in a:
+    response = table.query(
+        KeyConditionExpression=Key('az').eq(c_az)
+    )
+    for i in response['Items']:
+        c_price = "$ " + i['price']
 
-                        c_price="$ "+i
+        # c_price=current_az_price
 
-    print(c_az)
-    print(type(c_az))
-    print(c_region)
-    print(type(c_region))
-    print(c_instance_id)
+    print("p_id is -> " + str(p_id))
+    print(type(p_id))
+
+    print("p_az is -> " + p_az)
+    print(type(p_az))
+
+    print("p_price -> " + p_price)
+    print(type(p_price))
+
+    print("c_instance_id -> " + c_instance_id)
     print(type(c_instance_id))
-    print (message)
-    print(c_price)
-    print (type(c_price))
 
-    conn = pymysql.connect(host="xxxxxxxxxx", user="xxxx", passwd="xxxxxx",
+    print("c_az is -> " + c_az)
+    print(type(c_az))
+
+    print("c_price -> " + c_price)
+    print(type(c_price))
+
+    print("time is -> " + instance_time)
+    print(type(instance_time))
+
+    conn = pymysql.connect(host="xxxx.us-east-1.rds.amazonaws.com", user="mj", passwd="xxxx",
                            db="migration", connect_timeout=5)
     with conn.cursor() as cur:
 
-        cur.execute('insert into Route (instance_id, region, az, price) values(%s,%s,%s,%s)',
-                    (c_instance_id, c_region, c_az, c_price))
+        cur.execute("UPDATE Route SET old_price=%s WHERE id=%s", (p_price, int(p_id)))
+        cur.execute('insert into Route (instance_id, az, price,time) values(%s,%s,%s,%s)',
+                    (c_instance_id, c_az, c_price, instance_time))
         conn.commit()
 
     cur.close()
     conn.close()
-
 
     return 0
