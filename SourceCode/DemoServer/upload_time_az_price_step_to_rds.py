49f70c825db905a909a6308b833ea3912c88289b
mjaysonnn <smj8612@kookmin.ac.kr>
2019-02-20 15:55:42 +0900

    update code according to new rds

--- SourceCode/DemoServer/upload_time_az_price_step_to_rds.py
+++ SourceCode/DemoServer/upload_time_az_price_step_to_rds.py
@@ -1,20 +1,21 @@
 import pymysql
 import boto3
-import json
+from datetime import datetime
 
 # 0 AWS Resource Initialize
 client = boto3.client('dynamodb')
+test_time = datetime.now().strftime("%Y-%m-%d,%H:%M:%S")
 
 
 def lambda_handler(event, context):
     # 1 Get 3 Parameters from EC2
     param_az = str(event.get('az', 'us-east-1c'))
     param_step = str(event.get('step', 'Unknown'))
-    param_current_time = (event.get('current_time', 'Unknown'))
+    param_current_time = (event.get('current_time', test_time))
 
     price = ""
     # 2 From DynamoDB, get price associated with current instance
-    response = client.get_item(TableName='g2-instance', Key={'az': {'S': param_az}})
+    response = client.get_item(TableName='DeepSpotCloud-G2SpotInstance-Price', Key={'az': {'S': param_az}})
 
     for keys, values in response.iteritems():
         if keys == "Item":
@@ -26,11 +27,13 @@ def lambda_handler(event, context):
                         price = "$ " + i
 
     # 3 Upload Status to Mysql Spottable
-    mysql_spottable = pymysql.connect(host="mj3.xxxxxxxxx.us-east-1.rds.amazonaws.com", user="mj", passwd="xxxxxx",
-                                      db="g2instance", connect_timeout=5)
+    mysql_spottable = pymysql.connect(host="deepspotcloud.xxxx.us-west-2.rds.amazonaws.com",
+                                      user="deepspotcloud",
+                                      passwd="xxxx",
+                                      db="deeplearning_status", connect_timeout=5)
     with mysql_spottable.cursor() as cur:
 
-        cur.execute('insert into Spottable (Time, az, Price, step) values(%s,%s,%s,%s)',
+        cur.execute('insert into DeepLearningStatus (c_time, az, c_price, c_step) values(%s,%s,%s,%s)',
                     (param_current_time, param_az, price, param_step))
         mysql_spottable.commit()
 
