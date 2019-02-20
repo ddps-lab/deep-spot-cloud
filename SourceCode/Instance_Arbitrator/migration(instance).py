9b810486e59a393cc1d63c0527a264f4d0e2ad6c
mjaysonnn <smj8612@kookmin.ac.kr>
2019-02-20 15:47:30 +0900

    update api url

--- SourceCode/Instance_Arbitrator/migration(instance).py
+++ SourceCode/Instance_Arbitrator/migration(instance).py
@@ -9,64 +9,65 @@ interrupt_check_url = "http://169.254.169.254/latest/meta-data/spot/termination-
 
 
 def check_live_migration():
-    _buffer = cStringIO.StringIO()
+    buffer = cStringIO.StringIO()
     c = pycurl.Curl()
     az = get_availabilty_zone()
-    c.setopt(c.URL, 'https://fdp82l56bj.execute-api.us-east-1.amazonaws.com/fetch/migration?az=%22' + az + '%22')
-    c.setopt(c.WRITEDATA, _buffer)
+    c.setopt(c.URL,
+             'https://xxxxxx.execute-api.us-west-2.amazonaws.com/deploy/decide-live-migration?az=%22' + az + '%22')
+    c.setopt(c.WRITEDATA, buffer)
     c.perform()
     c.close()
-    body = _buffer.getvalue()
+    body = buffer.getvalue()
     return body
 
 
 def check_forced_migration():
-    _buffer = cStringIO.StringIO()
+    buffer = cStringIO.StringIO()
     c = pycurl.Curl()
     c.setopt(c.URL, interrupt_check_url)
-    c.setopt(c.WRITEDATA, _buffer)
+    c.setopt(c.WRITEDATA, buffer)
     c.perform()
     c.close()
-    body = _buffer.getvalue()
+    body = buffer.getvalue()
     return bool(re.search('.*T.*Z', body))
 
 
 def get_availabilty_zone():
-    _buffer = cStringIO.StringIO()
+    buffer = cStringIO.StringIO()
     c = pycurl.Curl()
     c.setopt(c.URL, 'http://169.254.169.254/2016-09-02/meta-data/placement/availability-zone')
-    c.setopt(c.WRITEDATA, _buffer)
+    c.setopt(c.WRITEDATA, buffer)
     c.perform()
     c.close()
-    body = _buffer.getvalue()
+    body = buffer.getvalue()
     return body
 
 
 def get_instance_id():
-    _buffer = cStringIO.StringIO()
+    buffer = cStringIO.StringIO()
     c = pycurl.Curl()
     c.setopt(c.URL, 'http://169.254.169.254/2016-09-02/meta-data/instance-id')
-    c.setopt(c.WRITEDATA, _buffer)
+    c.setopt(c.WRITEDATA, buffer)
     c.perform()
     c.close()
-    body = _buffer.getvalue()
+    body = buffer.getvalue()
     return body
 
 
 def make_migration_notice():
     instance_id = get_instance_id()
     path = "/tmp/" + instance_id
-    os.mkdir(path, 0755)
+    os.mkdir(path, 0755);
 
 
 def get_instance_launching_time():
-    _buffer = cStringIO.StringIO()
+    buffer = cStringIO.StringIO()
     c = pycurl.Curl()
-    c.setopt(c.URL, 'https://xxxxx.execute-api.us-east-1.amazonaws.com/fetch/')
-    c.setopt(c.WRITEDATA, _buffer)
+    c.setopt(c.URL, 'https://xxxxxxx.execute-api.us-west-2.amazonaws.com/deploy/instance-launch-time')
+    c.setopt(c.WRITEDATA, buffer)
     c.perform()
     c.close()
-    body = _buffer.getvalue()
+    body = buffer.getvalue()
     return body
 
 
@@ -91,7 +92,7 @@ while True:
         print "checking for migration - elapsed minutes"
         print elapsed_minutes
         result = check_live_migration()
-        if result == "true":
+        if (result == "true"):
             print "initiate migration(live)"
             make_migration_notice()
             break
@@ -99,7 +100,7 @@ while True:
     elif (elapsed_minutes - 55) % 60 != 0 and flag == 1:
         print "No migration yet"
         flag = 0
-    elif check_forced_migration():
+    elif (check_forced_migration()):
         make_migration_notice()
         print "initiate migration(forced)"
         break
